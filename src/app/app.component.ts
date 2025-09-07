32856181b5163dbc5de2e2ce423b94904a2b1dfc
Pedro <pedroroangelo2002@gmail.com>
2025-09-07 12:41:04 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, RouterLink, MatIconModule, MatButtonModule, MatToolbarModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
